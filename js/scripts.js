79cc84096be3214b1854cde89b30cec928707347
Mike Roberts <mroberts@endicott.edu>
2025-05-18 20:48:00 -0400

    initialize

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Declare Variables for Modal
+
+// The button to display the modal
+const showForm = document.getElementById("showformbutton");
+
+// The modal
+const joinMailingList = document.getElementById("joinmailinglist");
+
+// Function to show modal
+function showTheForm() {
+    joinMailingList.showModal();
+}
+
+// showForm.addEventListener("click", showTheForm);
